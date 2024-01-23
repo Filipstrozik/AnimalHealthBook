5d2fe81f0c4eb4aebce4efb4decd583933b8b556
Filipstrozik <filipshelby@gmail.com>
2024-01-23 22:54:56 +0100

    Refactor authentication and authorization setup

--- AnimalHealthBookApi/AnimalHealthBookApi/Program.cs
+++ AnimalHealthBookApi/AnimalHealthBookApi/Program.cs
@@ -22,8 +22,10 @@ builder.Services.AddDbContext<AHBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AHB")));
 
 
-builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
+builder.Services.AddAuthentication()
+    .AddBearerToken(IdentityConstants.BearerScheme);
 
+builder.Services.AddAuthorizationBuilder();
 
 builder.Services.AddIdentity<User, Role>()
     .AddEntityFrameworkStores<AHBContext>()
@@ -83,5 +85,44 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.MapIdentityApi<User>();
+
+using(var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<Role>>();
+
+    var roles = new[] { "Admin", "User" };
+
+    foreach (var role in roles)
+    {
+        if(!roleManager.RoleExistsAsync(role).Result)
+        {
+            var newRole = new Role
+            {
+                Name = role,
+                Description = $"This is the {role} role"
+            };
+            await roleManager.CreateAsync(newRole);
+        }
+    }
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+
+    string email = "admin@admin.com";
+    string password = "Admin123!";
+
+    if(await userManager.FindByEmailAsync(email) == null)
+    {
+        var user = new User();
+        user.Email = email;
+        user.UserName = email;
+
+        await userManager.CreateAsync(user, password);
+
+        await userManager.AddToRoleAsync(user, "Admin");
+    }
+}
 
 app.Run();
